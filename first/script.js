c6f35c1039f6a497cc81c1c2be70ca225a91b4b3
Shreeraj-Shivale <66412849+Shreeraj-Shivale@users.noreply.github.com>
2021-02-03 09:33:42 +0530

    fixed navbar,footer,added text stuff(need to add grid system for rules yet)

--- first/script.js
+++ first/script.js
@@ -1,34 +1,54 @@
-(function($) {
-    "use strict";
+// (function($) {
+//     "use strict";
 
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 60
-    });
+//     $('body').scrollspy({
+//         target: '.navbar-fixed-top',
+//         offset: 60
+//     });
 
-    $('#topNav').affix({
-        offset: {
-            top: 200
-        }
-    });
-    
-    new WOW().init();
-    
+//     $('#topNav').affix({
+//         offset: {
+//             top: 200
+//         }
+//     });
+
+//     new WOW().init();
+
+//     $('a.page-scroll').bind('click', function(event) {
+//         var $ele = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: ($($ele.attr('href')).offset().top - 60)
+//         }, 1450, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+
+//     $('.navbar-collapse ul li a').click(function() {
+//         /* always close responsive nav after click */
+//         $('.navbar-toggle:visible').click();
+//     });
+
+//     $('#galleryModal').on('show.bs.modal', function (e) {
+//        $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+//     });
+
+// })(jQuery);
+
+$(function() {
     $('a.page-scroll').bind('click', function(event) {
-        var $ele = $(this);
+        var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($ele.attr('href')).offset().top - 60)
-        }, 1450, 'easeInOutExpo');
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-    
-    $('.navbar-collapse ul li a').click(function() {
-        /* always close responsive nav after click */
-        $('.navbar-toggle:visible').click();
-    });
+});
 
-    $('#galleryModal').on('show.bs.modal', function (e) {
-       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
-    });
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
 
-})(jQuery);
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
